aad590d4a9878b1ee3753579add9e44342963a87
Ying Huang <huangying270@gmail.com>
2019-08-01 19:52:13 -0400

    included cli table, table displays on bash

--- bamazon.js
+++ bamazon.js
@@ -1,18 +1,44 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  port: 3306,
+    port: 3306,
 
-  user: "root",
+    user: "root",
 
-  password: "",
-  database: "bamazon_DB"
+    password: "",
+    database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    start();
-  });
+    displayInventory();
+});
+
+function displayInventory() {
+    var query = "SELECT * FROM items";
+    connection.query(query, function (err, res) {
+        if(err) throw err;
+        var displayTable = new Table ({
+            head: ["Item ID", "Item Name", "Category", "Price", "Stock Quantity"],
+            colWidth: [10,20,20,10,10]
+        });
+        for(var i = 0; i < res.length; i++) {
+            displayTable.push([res[i].item_id, res[i].item_name, res[i].category, res[i].price, res[i].stock]);
+        }
+        console.log(displayTable.toString());
+    });
+    
+}
+
+/* CREATE TABLE items (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INTEGER(10),
+    PRIMARY KEY (item_id)
+  ); */
